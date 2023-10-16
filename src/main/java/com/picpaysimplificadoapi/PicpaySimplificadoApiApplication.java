1fdce5099803cf38a739bc79655ba3ac8a059f7c
Witor Sather <satherws@gmail.com>
2023-10-15 21:26:43 -0300

    feat: initial config

---
+++ src/main/java/com/picpaysimplificadoapi/PicpaySimplificadoApiApplication.java
@@ -0,0 +1,13 @@
+package com.picpaysimplificadoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PicpaySimplificadoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PicpaySimplificadoApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/picpaysimplificadoapi/PicpaySimplificadoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.picpaysimplificadoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PicpaySimplificadoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
